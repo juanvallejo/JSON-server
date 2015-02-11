66ad16fe5f191614095d8a15dad1c1e56348eace
David <dkroell@acm.org>
2015-02-10 21:33:22 -0500

    First Testcase
    Runs the POST method defined in the server.js file that is in the JSON-server repo.

---
+++ TEST_CASES/UAS_TestCase1.js
@@ -0,0 +1,16 @@
+var http = require('http');
+ 
+// our main function. Runs on program start
+(function main(argv) {
+    
+    var netRequest = http.request({
+        method:'POST',
+        host:'127.0.0.1',
+        port:'8080'
+    }, function(response) {
+        //console.log that mavlink listener has been tested
+    });
+    
+    netRequest.end("This is where you type the data you want to send");
+    
+})(process.argv);
