79414c4b6cd515caf417a7b5904fad6bae8ba235
David <dkroell@acm.org>
2015-02-21 20:17:24 -0500

    Adding the obstacle information test outline.
    Please note I decided to put this in my branch and the master, while most of the pushes will be in to branch, DiZ. I will generate pull requests from here on.

---
+++ tests/getObsticleInformationTest.js
@@ -0,0 +1,28 @@
+#!/bin/env node
+
+/**
+ * Provided under the MIT License (c) 2014
+ * See LICENSE @file for details.
+ *
+ * Files written to test interoperability request handling for the UAS competition server
+ *
+ * @file postUASTelemetryTest.js
+ *
+ * @author juanvallejo
+ * @date 2/9/14
+ */
+
+// define libraries to be used 
+
+var http = require('http');
+
+/**
+Checks to see if Obsticle information is handled correctly by the JSON-server, as a GET request on the server. 
+*/
+
+(function doesJSONServerHandleObsticleInformation() {
+
+	var postData        = 'this=is&a=test'
+	
+
+});
